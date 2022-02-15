04a9b62c0706238c9ca17de4658dc93d3a009847
grifinchek <dlysenko748@gmail.com>
2022-02-15 13:53:51 +0200

    last commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      indicators: true
+    });
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  });
+  
+ 
+  
+ 
+  
